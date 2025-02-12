32360d71f33e27235fdb9b9aec1d0401108a54d8
Akash-Ahamed <akash.ahamed.engr@gmail.com>
2025-02-12 22:27:11 +0600

    Added verify email

---
+++ src/app/api/users/verifyEmail/route.js
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "../../../../models/userModel";
+import { connectMongoDB } from "../../../../lib/db";
+
+connectMongoDB();
+
+export async function POST(request) {
+  try {
+  } catch (error) {}
+}
